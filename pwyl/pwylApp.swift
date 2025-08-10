2de2a9c109d22daab61b2376d8b7d00d43ae5576
drake <drakew@gmail.com>
2025-08-09 16:12:50 -1000

    Initial Commit

---
+++ pwyl/pwylApp.swift
@@ -0,0 +1,17 @@
+//
+//  pwylApp.swift
+//  pwyl
+//
+//  Created by Eric Weinert on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct pwylApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
